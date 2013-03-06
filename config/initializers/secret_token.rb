cbdbf338a1ea190467aa5c7820f33fe72abb1d2f
Carlos Cardona <cgcardona@gmail.com>
2013-03-06 14:09:53 -0800

    stub rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cardonadesigns::Application.config.secret_token = '7deafe3ae9fd1b7cbfee3e7ca3c131ecba655eb0cd48102f2b8ad5270a21689179f81d5138020c09749e8eb1e480c63971c188c50d7a5936f857d6713e4305f6'
